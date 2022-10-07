0572e8d7ddc1e0e29d442f37bc2e84fa03000455
Austin Pereira <79310192+AUSt-in@users.noreply.github.com>
2022-10-06 17:43:46 -0700

    Create search_in_sorted_rotated_array.java

---
+++ BinarySearch/search_in_sorted_rotated_array.java
@@ -0,0 +1,34 @@
+public boolean search(int[] nums, int target) {
+        int start = 0, end = nums.length, mid =-1;
+        while(start<= end){
+            mid = (start+end)/2;
+            if(nums[mid] == target){
+                return true;
+                
+            }
+            if(nums[mid] < nums[end] || nums[mid ] < nums[start]){
+                if(target > nums[mid] && target <=nums[end]){
+                    start = mid+1;
+                    
+                }
+                else{
+                    end = mid -1;
+                }
+            }
+            else if(nums[mid] >nums[start] || nums[mid] > nums[end]){
+                if(target< nums[mid] && target >= nums[start])
+                {
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+                
+            }
+            else{
+                end--; // if two values are equal
+            }
+        }
+        
+        return false;
+}
